2c9a84306d215b573b1e0d3c627896e5488dffae
mariusz17 <qbajt.mariusz.kubat@gmail.com>
2022-05-06 22:18:15 +0200

    add useRef to highlight item to delete

--- src/components/EmployeeItem.jsx
+++ src/components/EmployeeItem.jsx
@@ -1,30 +1,29 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useRef } from "react";
 import EmployeeContext from "../context/EmployeeContext";
 
 function EmployeeItem({ item }) {
 	const { deleteEmployee } = useContext(EmployeeContext);
 	const [isHighlighted, setIsHighlighted] = useState(false);
+	const li = useRef();
 
-	const highLightOn = () => {
-		setIsHighlighted(true);
-	};
-
-	const highLightOff = () => {
-		setIsHighlighted(false);
-	};
-
+	const toggleHighlight = () => {
+		li.current.classList.toggle("highlighted");
+	}
 	return (
 		<li
+			ref={li}
 			data-id={item.id}
-			className={`employee-item ${isHighlighted && "highlighted"}`}
+			className="employee-item"
 		>
 			<p className="employee-data">
 				{item.name} {item.surname}
 			</p>
 			<button
 				onClick={() => deleteEmployee(item.id)}
-				onMouseOver={highLightOn}
-				onMouseOut={highLightOff}
+				// onMouseOver={() => setIsHighlighted(true)}
+				// onMouseOut={() => setIsHighlighted(false)}
+				onMouseOver={toggleHighlight}
+				onMouseOut={toggleHighlight}
 				className="employee-delete"
 			>
 				Delete
